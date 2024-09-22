54573fdd290425e4a01c93fd456a04f350d3b833
Tim Neunherz <tim.neunherz@outlook.de>
2024-09-22 20:09:51 +0200

    Created scenes: - game - home - hunter
    Created scripts:
    - huntrer
    
    Created tileset:
    - indoor

---
+++ assets/scripts/hunter.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+
+
+const SPEED = 150.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	
+	var directionX := Input.get_axis("move_left", "move_right")
+	var directionY := Input.get_axis("move_up", "move_down")
+	
+	if directionX or directionY: 
+		var diag = 1
+		if directionY:
+			diag = 0.75
+		velocity.x = directionX * SPEED * diag
+		velocity.y = directionY * SPEED * diag
+			
+	else:
+		velocity.x = 0
+		velocity.y = 0
+
+	move_and_slide()
